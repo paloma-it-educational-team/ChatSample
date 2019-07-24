f7136e0d6217bca2e48e9d002451845e40cdfa7f
Shingo Morimoto <shingo.morimoto@paloma.co.jp>
2019-07-25 00:34:09 +0900

    new

--- ChatSample/Form1.Designer.cs
+++ ChatSample/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(766, 283);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "時刻";
+            this.columnHeader1.Width = 101;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "名前";
+            this.columnHeader2.Width = 145;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "メッセージ";
+            this.columnHeader3.Width = 426;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 315);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(571, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(608, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "送信";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(790, 372);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Chat";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ChatSample/Form1.cs
+++ ChatSample/Form1.cs
@@ -15,6 +15,52 @@ namespace ChatSample
         public Form1()
         {
             InitializeComponent();
+            var t = Task.Factory.StartNew(RecieveLoop);
+        }
+        System.Net.Sockets.UdpClient udp =
+                        new System.Net.Sockets.UdpClient(9999);
+        delegate void ListViewEdit(ListViewItem item);
+        void RecieveLoop()
+        {
+            while (true)
+            {
+                System.Net.IPEndPoint remoteEP = null;
+                byte[] rcvBytes = udp.Receive(ref remoteEP);
+                string rcvMsg = System.Text.Encoding.UTF8.GetString(rcvBytes);
+                var msg = Newtonsoft.Json.JsonConvert.DeserializeObject<Message>(rcvMsg);
+                var item = new ListViewItem(DateTime.Now.ToShortTimeString());
+                item.SubItems.Add(msg.Name);
+                item.SubItems.Add(msg.Body);
+                if (item.Name != System.Environment.UserName)
+                {
+                    Invoke(new ListViewEdit(CalcListView), item);
+                    //listView1.Items.Add(item);//これだとエラー
+                }
+
+            }
+        }
+        void CalcListView(ListViewItem item)
+        {
+            listView1.Items.Add(item);
+        }
+
+        class Message
+        {
+            public string Name { get; set; }
+            public string Body { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var obj = new Message()
+            {
+                Name = System.Environment.UserName,
+                Body = textBox1.Text
+            };
+            string sendMsg = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(sendMsg);
+            udp.Send(sendBytes, sendBytes.Length, "192.168.10.255", 9999);
+            textBox1.Text = "";
         }
     }
 }
